ab306faa14c87681360e73e07e2e79e46ea6e9d3
Augusto <augusto@britecore.com>
2020-11-16 10:13:44 -0300

    Renders page with Vuejs

--- collab/core/views.py
+++ collab/core/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from collab.core.models import Document
 from collab.core.forms import DocumentForm
 from django.views.decorators.csrf import csrf_exempt
+from django.core.serializers import serialize
 
 
 @csrf_exempt
 def document(request):
-    document = Document.objects.all().first()
+    documents = Document.objects.all()
     if request.method == 'POST':
-        form = DocumentForm(request.POST, instance=document)
+        form = DocumentForm(request.POST, instance=documents.first())
         if form.is_valid():
             form.save()
-    return render(request, 'core/document.html', {'document': document})
+    return render(request, 'core/document.html', {'document': serialize('json', documents)})
